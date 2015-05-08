fd601ef89830d30656bd04eeae90712b458f1d24
Henry <henrylu518@gmail.com>
2015-05-08 19:02:08 +0800

    add comment

--- Valid Palindrome.py
+++ Valid Palindrome.py
@@ -1,3 +1,21 @@
+"""
+ Author:     Henry, henrylu518@gmail.com
+ Date:       May 8, 2015
+ Problem:    Valid Palindrome
+ Difficulty: Easy
+ Source:     http://leetcode.com/onlinejudge#question_125
+ Notes:
+ Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+ For example,
+ "A man, a plan, a canal: Panama" is a palindrome.
+ "race a car" is not a palindrome.
+ Note:
+ Have you consider that the string might be empty? This is a good question to ask during an interview.
+ For the purpose of this problem, we define empty string as valid palindrome.
+
+ Solution: traverse from both side.
+ """
+
 class Solution:
     # @param {string} s
     # @return {boolean}
